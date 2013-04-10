d530b2417a857d2543c85cbbc79389ec7281e1a5
TigerWolf <hiddentiger@gmail.com>
2013-04-11 00:48:06 +0930

    Comment out proxy and test using streamer

--- test.rb
+++ test.rb
@@ -3,8 +3,13 @@ require 'excon'
 
 require 'net/http' # for Gzip and Zlib
 
-connection = Excon.new('http://geemus.com', :proxy => 'http://127.0.0.1:8888')
-c = connection.request(:method => 'GET')#, :headers => {'Accept-Encoding' => 'gzip, deflate'})
+streamer = lambda do |chunk, remaining_bytes, total_bytes|
+  puts chunk
+  puts "Remaining: #{remaining_bytes.to_f / total_bytes}%"
+end
+
+connection = Excon.new('http://geemus.com', :response_block => streamer) #:proxy => 'http://127.0.0.1:8888',
+c = connection.request(:method => 'GET', :headers => {'Accept-Encoding' => 'gzip, deflate'})
 puts c.headers
 if c.headers['Content-Encoding'] == 'gzip'
 	sio = StringIO.new( c.body )
